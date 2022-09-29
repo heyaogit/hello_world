2d418f562d8da3c374d42cd7f0b735f685508053
heyaogit <384415026@qq.com>
2022-09-29 22:56:39 +0800

    this is first practice commit

---
+++ src/main/java/com/example/newcommit/NewcommitApplication.java
@@ -0,0 +1,13 @@
+package com.example.newcommit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcommitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewcommitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newcommit/NewcommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newcommit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewcommitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
